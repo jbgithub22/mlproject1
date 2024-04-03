907d31beac7b2ee7e25539a691cfb031d7327a8e
jbgithub22 <jb.github22@gmail.com>
2024-04-03 17:47:15 +0800

    Hyperparameter tuning.

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -60,12 +60,64 @@ class ModelTrainer:
                 "XGBRegressor": XGBRegressor(), 
             }
 
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Lasso":{
+                    'alpha': [0.001, 0.01, 0.1, 1, 10]
+                },
+                "Ridge":{
+                    'alpha': [0.001, 0.01, 0.1, 1, 10]
+                },
+                "K-Neighbors Regressor":{
+                    # Number of neighbors to consider
+                    'n_neighbors': [3, 5, 7, 9, 11],  
+                    # Weighting method for neighbors
+                    'weights': ['uniform', 'distance'],  
+                    # Minkowski distance parameter (1 for Manhattan, 2 for Euclidean)
+                    'p': [1, 2]  
+                },
+            }
+
             model_report:dict = evaluate_models(
                 X_train = X_train, 
                 y_train = y_train,
                 X_test = X_test,
                 y_test = y_test,
-                models = models
+                models = models,
+                params = params,
             )
             
             ## Obtain best model score from dict
